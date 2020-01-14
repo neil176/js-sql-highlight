71e78e835028f1a5b7ced6bec2146e98d6839549
Neil Steiner <npsteiner@gmail.com>
2020-01-14 05:34:20 -0500

    Remove copies and unnecessary test files

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
+Forked from https://github.com/mydesireiscoma/es6-string-html but significantly changed to support the following behavior:
 
-Forked from https://github.com/mydesireiscoma/es6-string-html
+- All consideration for html removed. This extension is just for highlighting sql in js
+- no flags or comments are required.
+- Any variable `sql` assigned a template string value should apply sql syntax highlighting to that value
 
-Purpose:
-- confine to sql
-- remove requirement to prefix string with a comment
-- Infer from key or var named "sql" that it should be highlighted
+```js
+const sql = `
+  INSERT INTO test
+    thing, otherthing, otherother
+  VALUES
+    ('test', 2, '${String(true) + "inside an interpolated value"}');
+    $2
+`;
+```
+
+- Any object key `sql` assigned a template string value should apply sql syntax highlighting to that value
+
+```js
+const mything = {
+  sql: `
+    SELECT
+    potato
+    FROM
+    mytable
+    WHERE id IN (1, 2, 3);
+  `
+};
+```
+
+### Would be nice to have:
+
+- Settings-defined var/key names on which to highlight
--- test.js
+++ test.js
@@ -1,183 +1,60 @@
-// Make sure default strings are not broken
-const defaultString2 = `--test
-String;`;
-const defaultString3 = `
-  If the first line is a string, it will be intepritied as a comment
-  sdfsdfdsfsdf
-  asdaskjdlasjdlkasjdas
-  dasdajslkdjaslkdjalskdjasd
-  asdaslkdjaksldasdasdasdas;
-`;
-
-const defaultString4 = `
-  nothing to highlight;
-`;
-
-const value = `Nothing can be here;`;
-const value2 = `<div>Something</div>`;
-
-const defaultString4 = `
-  asdasdas
-  asdasdasdasdajsdlasdjklasjdlajsdlasjdlasjdlasjdlasjldajsldkjasldkajsldakjsdl
-  <div attribute attribute="value" lastattr>asdasd</div>
-  sadasdasdasd
-  asdasdas
-  asdasdasda
-  sdasdasdasdasdasd
-  asdasdasdasdasdasda
-  sdasdasdasdsadasdasdasd
-  asdasdasdasdasdlkjasldjalskdjalskdjalsdjalksdjalskdj
-`;
-
-// Check single-line HTML highlighting
-const singleLine = `<div>${div}Something</div>`;
-
-function test() {
-  return true;
-}
-
-const singleLineObj = {
-  prop: test(""),
-  noHTML: `nohtml`
-};
-test(`hey`, `1`);
-callFn("arg", `<div attr="asdasd">Value</div>`);
-const test = "thing";
-
-const multilineObj = {
-  prop: `
-        <div att="adsasd"> ${test("potato")}</div>
-`,
-  asd: "sadasdasd"
-};
-
-// Check functions
-const checkFn = temp(`<div attr="value">Message</div>`);
-
-// Misc
-function test() {
-  const result = true;
-  return result;
-}
-
-const stress1 = `asdasdasd`;
-const stress2 = `<no>asd<div attr="asdad"></div>adasd</no>`;
-const stress3 = `<no> ${test("asdasd")} </no>`;
-
-function test() {
-  const result = true;
-  return result;
-}
-
-const testSQL = /* sql */ `
-    -- THIS FILE SHOWS EXAMPLE SYNTAX HIGHLIGHTING
-    -- FOR SQL
-    INSERT INTO database.table (1, 2, 3) VALUES (1, 2, 3);
-    CREATE TABLE mytable;
-    DELETE FROM mytable WHERE id = 2
-`;
-
-const othrtestsql = `
-    -- THIS FILE SHOWS EXAMPLE SYNTAX HIGHLIGHTING
-    -- FOR SQL
-    INSERT INTO database.table (1, 2, 3) VALUES (1, 2, 3);
-    CREATE TABLE mytable;
-    DELETE FROM mytable WHERE id = 2
-`;
-
+// Highlight the string value for any key 'sql'
 const mything = {
   sql: `
     SELECT
     potato
     FROM
     mytable
-    WHERE id IN (1, 2, 3);ß
-`
+    WHERE id IN (1, 2, 3);
+  `
 };
 
+// Highlight the string value for any var 'sql'
+// While still highlighting interpolation correctly
 const sql = `
   INSERT INTO test
-    thing, otherthing
+    thing, otherthing, otherother
   VALUES
-    ('test', 2);
-    limit ${thing}
+    ('test', 2, '${String(true) + "inside an interpolated value"}');
     $2
 `;
 
-function imapickle(test = `hey`) {
-  return Boolean(test);
+// Should not care about being multiline
+function newScopeForMoreSql() {
+  const sql = `SELECT something FROM nothing;`;
+  const a = function furtherNestedSqlShadowing() {
+    const sql = "Not a template string";
+  };
 }
 
-/*
-        {
-          "name": "string.quoted.template.js",
-          "match": "^\\s*([^\\<]*)\\s*$"
-        },
-        
- {
-  "fileTypes": [
-    "js",
-    "jsx",
-    "ts",
-    "tsx",
-    "html"
-  ],
-  "injectionSelector": "L:source.js -comment -string, L:source.js -comment -string, L:source.jsx -comment -string,  L:source.js.jsx -comment -string, L:source.ts -comment -string, L:source.tsx -comment -string",
-  "injections": {
-    "L:source": {
-      "patterns": [
-        {
-          "match": "<",
-          "name": "invalid.illegal.bad-angle-bracket.html"
-        }
-      ]
-    }
-  },
-  "patterns": [
-    {
-      "comment": "Single line strings",
-      "begin": "(\\=|\\:|\\,|\\>)\\s*(`)$",
-      "beginCaptures": {
-        "1": {
-          "name": "keyword.operator.assignment.js"
-        },
-        "2": {
-          "name": "string.quoted.template.js"
-        }
-      },
-      "end": "\\s*(`)($)",
-      "endCaptures": {
-        "1": {
-          "name": "string.quoted.template.js"
-        }
-      },
-      "patterns": [
-        {
-          "comment": "It's matches only tags but not anything inside",
-          "begin": "(<\/?)\\!?(\\w[^\\s>]*)(?<!/)",
-          "end": "((?: ?/)?>)",
-          "beginCaptures": {
-            "1": {
-              "name": "punctuation.definition.tag.begin.html"
-            },
-            "2": {
-              "name": "entity.name.tag.html"
-            }
-          },
-          "endCaptures": {
-            "1": {
-              "name": "punctuation.definition.tag.end.html"
-            }
-          },
-          "patterns": [
-            {
-              "include": "text.html.basic#attribute"
-            }
-          ]
-        }
-      ]
-    }
-  ],
-  "scopeName": "inline.es6-html"
+// Do not affect anything else
+const singleLineString = "SELECT not_sql FROM my_table;";
+const multiLineString = `
+  asdasdas
+  asdasdasdasda
+  sadasdasdasd;
+`;
+
+function testFunc() {
+  for (let i = 0; i < a.length; i++) {
+    const element = a[i];
+    console.log(element);
+  }
+  return true;
 }
-*/
+
+function pickles(testParam = `hey`) {
+  return Boolean(testParam);
+}
+
+const testObject = {
+  prop: testFunc(""),
+  someKey: `
+    my multiline string
+    starts here;
+    and ends here;
+  `,
+  notsql: `
+    hmmm
+  `
+};
